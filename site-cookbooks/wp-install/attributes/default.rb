2cbc41ad7f59b35f7a151a4b28efda5b9b1646ed
takatoshi <kobayashi.takatoshi@gmail.com>
2014-01-12 00:16:48 +0900

    Added attributes in which some values are defined about live and debug mode

---
+++ site-cookbooks/wp-install/attributes/default.rb
@@ -0,0 +1,12 @@
+case node['mode']
+when "live"
+  default['nginx']['do_not_cache'] = 0
+  default['nginx']['expire'] = "10d"
+  default['nginx']['gzip'] = "on"
+  default['nginx']['gzip_static'] = "on"
+when "debug"
+  default['nginx']['do_not_cache'] = 1
+  default['nginx']['expire'] = "off"
+  default['nginx']['gzip'] = "off"
+  default['nginx']['gzip_static'] = "off"
+end
