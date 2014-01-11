23b41573bee639e89c001571637988b080253c3b
takatoshi <kobayashi.takatoshi@gmail.com>
2014-01-12 01:09:01 +0900

    Added sendfile value to Attributes

--- site-cookbooks/wp-install/attributes/default.rb
+++ site-cookbooks/wp-install/attributes/default.rb
@@ -4,9 +4,11 @@ when "live"
   default['nginx']['expire'] = "10d"
   default['nginx']['gzip'] = "on"
   default['nginx']['gzip_static'] = "on"
+  default['nginx']['sendfile'] = "on"
 when "debug"
   default['nginx']['do_not_cache'] = 1
   default['nginx']['expire'] = "off"
   default['nginx']['gzip'] = "off"
   default['nginx']['gzip_static'] = "off"
+  default['nginx']['sendfile'] = "off"
 end
