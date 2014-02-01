28fdbbf239039ffe6c35111993918d5d07edc138
takatoshi <kobayashi.takatoshi@gmail.com>
2014-02-01 23:55:07 +0900

    Add attribute of pagespeed

--- site-cookbooks/wp-install/attributes/default.rb
+++ site-cookbooks/wp-install/attributes/default.rb
@@ -5,10 +5,12 @@ when "live"
   default['nginx']['gzip'] = "on"
   default['nginx']['gzip_static'] = "on"
   default['nginx']['sendfile'] = "on"
+  default['nginx']['pagespeed'] = "on"
 when "debug"
   default['nginx']['do_not_cache'] = 1
   default['nginx']['expire'] = "off"
   default['nginx']['gzip'] = "off"
   default['nginx']['gzip_static'] = "off"
   default['nginx']['sendfile'] = "off"
+  default['nginx']['pagespeed'] = "off"
 end
